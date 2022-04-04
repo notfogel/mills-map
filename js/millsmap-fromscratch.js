f51ed164eeb472f2710834ff087bf2a1d050f63c
Sammy <98495588+notfogel@users.noreply.github.com>
2022-04-04 11:21:15 -0500

    a working map!!!!!!!!!!!!

--- js/millsmap-fromscratch.js
+++ js/millsmap-fromscratch.js
@@ -14,28 +14,46 @@ function createMap(){
     getData(map);
 };
 
-function getData(){
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
+};
 
-    fetch("data/mills-example-places.geojson") 
-    .then(function(response){
-        return response.json();
-})
-    .then(function(json){
-        L.geoJSON(json),addTo(map);
+function getData(){
+    //load that data!!!
+    fetch("data/mills-example-try2.geojson") 
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            var geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+    
+            //(in theory) create a geoJSON layer and add it to the map
+            L.geoJSON(json, {
+                pointToLayer: function (feature, latlng) {
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                },        
+                onEachFeature: onEachFeature
+    
+            }).addTo(map);
         //doin some attribute stuff with arrays or something
         //var attributes = processData(json);
         //console.log(attributes);
     })
 };
-
-
-
-
-
-
-
-
-
-
 
 document.addEventListener('DOMContentLoaded',createMap)
